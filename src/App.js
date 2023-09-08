7eda1fed7ec802f0520912f484b110f4e5c29c29
Ashley Davis <ashley@codecapers.com.au>
2023-09-08 20:27:50 +1000

    Factored out a context.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,99 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState, createContext, useContext } from 'react';
+
+const DragContext = createContext(null);
+
+
+export function DragContextProvider({ children }) {
+
+    const [draggingItem, setDraggingItem] = useState(undefined);
+    const [mousePos, setMousePos] = useState(undefined);
+
+    function onMouseMove(event) {
+        setMousePos({ x: event.clientX, y: event.clientY });
+
+        if (draggingItem) {
+            console.log('draggingItem');
+
+            //todo: are we over something?
+        }
+    };
+
+    useEffect(() => {
+        //
+        // Handles mouse move on the window.
+        //
+        document.addEventListener('mousemove', onMouseMove);
+
+        return () => {
+            document.removeEventListener('mousemove', onMouseMove);
+        };
+
+    }, []);
+
+    const value = {
+        draggingItem, 
+        setDraggingItem,
+        mousePos,
+        setMousePos,
+    };
+
+    return (
+        <DragContext.Provider
+            value={value}
+            >
+            {children}
+        </DragContext.Provider>
+    );
+}
+
+function useDraggable() {
+    return useContext(DragContext);
+}
+
+function Item({ item }) {
+
+    const { setDraggingItem } = useDraggable();
+
+    return (
+        <div
+            className="m-1 p-1 border-2 border-solid border-gray-600 bg-white w-48 h-48"
+            onMouseDown={e => {
+                console.log('onMouseDown');
+                console.log(e);
+
+                // Starts dragging.
+                setDraggingItem(Object.assign({}, item)); //todo: move this into draggable.
+            }}
+            >
+            {item.name}
+        </div>
+    );
+}
 
 function App() {
+
+    const { draggingItem, mousePos } = useDraggable();
+
     const items1 = [
         { id: 1, name: 'item 1' },
     ];
     const items2 = [
+        { id: 2, name: 'item 2' },
     ];
-    const [draggingItem, setDraggingItem] = useState(undefined);
-    const [mousePos, setMousePos] = useState(undefined);
     return (
-        <div 
+        <div
             className="flex flex-row w-full h-screen relative"
-            onMouseMove={e => {
-                // console.log('onMouseMove');
-                // console.log(e);
-
-                setMousePos({ x: e.clientX, y: e.clientY });
-            }}
             >
             <div className="flex flex-col flex-grow bg-green-200 h-full">
                 {items1.map(item => (
-                    <div
-                        key={item.id}
-                        className="m-1 p-1 border-2 border-solid border-gray-600 bg-white w-48 h-48"
-                        onMouseDown={e => {
-                            console.log('onMouseDown');
-                            console.log(e);
-
-                            // Starts dragging.
-                            setDraggingItem(Object.assign({}, item));
-                        }}
-                    >
-                        {item.name}
-                    </div>
+                    <Item key={item.id} item={item} />
                 ))}
             </div>
+
             <div className="flex flex-col flex-grow bg-blue-200 h-full">
                 {items2.map(item => (
-                    <div
-                        key={item.id}
-                        className="m-1 p-1 border-2 border-solid border-gray-600 bg-white w-48 h-48"
-                        onMouseDown={e => {
-                            //todo: this should be on all draggables!
-                            console.log('onMouseDown');
-                            console.log(e);
-                        }}
-                    >
-                        {item.name}
-                    </div>
+                    <Item key={item.id} item={item} />
                 ))}
             </div>
 
