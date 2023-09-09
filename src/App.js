183a1825bfb949eb411427e32ba79ac65ef65ce6
Ashley Davis <ashley@codecapers.com.au>
2023-09-09 15:51:32 +1000

    Restructure to create separate hooks for context, draggables and droppables.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import { useEffect, useState, createContext, useContext, useRef, useCallback } from 'react';
+import { useEffect, useState, createContext, useContext, useRef } from 'react';
 
 const DragContext = createContext(null);
 
-
-export function DragContextProvider({ children }) {
+export function DragContextProvider({ onDragStart, onDragOver, onDragEnd, onDragCancel, children }) { //todo: rename me
 
     const [draggingItem, setDraggingItem] = useState(undefined);
     const [mousePos, setMousePos] = useState(undefined);
     const droppableRefs = useRef({}); 
+    const dragOverRef = useRef(undefined);
 
     useEffect(() => {
         if (!draggingItem) {
@@ -19,27 +19,37 @@ export function DragContextProvider({ children }) {
             setMousePos({ x: event.clientX, y: event.clientY });
     
             if (draggingItem) {
-                if (droppableRefs.current) {
-                    for (const [id, droppable] of Object.entries(droppableRefs.current)) {
-                        const droppableRect = droppable.getBoundingClientRect();
-
-                        //TODO: closest corner would be better.
-
-                        // If the mouse position is within the droppable area.
-                        if (event.clientX >= droppableRect.left && event.clientX <= droppableRect.right &&
-                            event.clientY >= droppableRect.top && event.clientY <= droppableRect.bottom) {
-                            console.log('hit droppable:');
-                            console.log(id);
-                        }
+                const droppable = determineDragOver(event);
+                if (droppable) {
+                    if (droppable.id !== dragOverRef.current?.id) {
+                        dragOverRef.current = droppable;
+                        onDragOver({
+                            active: draggingItem,
+                            over: droppable,
+                        });
                     }
                 }
             }
         }
         
         function onMouseUp(event) {
-            setDraggingItem(undefined);
+            const draggedItem = draggingItem;
+            const droppable = determineDragOver(event);
 
-            //todo: trigger drop event.
+            setDraggingItem(undefined);
+            dragOverRef.current = undefined;
+
+            if (droppable) {
+                onDragEnd({
+                    active: draggedItem,
+                    over: droppable,
+                });
+            }
+            else {
+                onDragCancel({
+                    active: draggedItem,
+                });
+            }
         }
 
         document.addEventListener('mousemove', onMouseMove);
@@ -52,21 +62,49 @@ export function DragContextProvider({ children }) {
 
     }, [draggingItem]);
 
-    function setDroppableRef(id, ref) {
-        // console.log('setDroppableRef'); 
-        // console.log(id);
-        // console.log(ref);
+    //
+    // Determine which droppable the mouse is over.
+    //
+    function determineDragOver(mouseEvent) {
+        if (droppableRefs.current) {
+            for (const [id, droppable] of Object.entries(droppableRefs.current)) {
+                const droppableRect = droppable.el.getBoundingClientRect();
 
+                //TODO: closest corner would be better.
+                // If the mouse position is within the droppable area.
+                if (mouseEvent.clientX >= droppableRect.left && mouseEvent.clientX <= droppableRect.right &&
+                    mouseEvent.clientY >= droppableRect.top && mouseEvent.clientY <= droppableRect.bottom) {
+                    return { id, droppable };
+                }
+            }
+        }
+
+        return undefined;
+    }
+
+    function initiateDragging(data) {
+        setDraggingItem(data);
+        onDragStart({
+            data,
+        });
+    }
+
+    function registerDroppable(id, el, data) {
         droppableRefs.current = {
             ...droppableRefs.current,
-            [id]: ref,
+            [id]: { el, data },
         };
+    }
+
+    function unregisterDroppable(id) {
+        delete droppableRefs.current[id];
     }
 
     const value = {
         draggingItem, 
-        setDraggingItem,
-        setDroppableRef,
+        initiateDragging,
+        registerDroppable,
+        unregisterDroppable,
         mousePos,
         setMousePos,
     };
@@ -80,25 +118,79 @@ export function DragContextProvider({ children }) {
     );
 }
 
-function useDraggable() {
+function useDragContext() {
     return useContext(DragContext);
+}
+
+function useDraggable({ id, data }) {
+
+    const { initiateDragging } = useDragContext();
+    const nodeRef = useRef(undefined);
+
+    useEffect(() => {
+        function onMouseDown(event) {
+            //tdoo: only initiate dragging when moved a certain distance.
+            initiateDragging(data); //todo: when do we clone the data?
+        }
+
+        if (nodeRef.current) {
+            nodeRef.current.addEventListener('mousedown', onMouseDown);
+        }
+
+        return () => {
+            if (nodeRef.current) {
+                nodeRef.current.removeEventListener('mousedown', onMouseDown);
+            }
+        };
+    }, []);
+
+    function setNodeRef(el) {
+        nodeRef.current = el;
+    }
+
+    return {
+        setNodeRef,
+    };
+}
+
+function useDroppable({ id, data }) {
+
+    const { registerDroppable, unregisterDroppable } = useDragContext();
+    const nodeRef = useRef(undefined);
+
+    useEffect(() => {
+        if (nodeRef.current) {
+            registerDroppable(id, nodeRef.current, data);
+        }
+
+        return () => {
+            if (nodeRef.current) {
+                unregisterDroppable(id);
+            }
+        };
+    }, []);
+
+    function setNodeRef(el) {
+        nodeRef.current = el;
+    }
+
+    return {
+        setNodeRef,
+    };
 }
 
 function Item({ item }) {
 
-    const { setDraggingItem, setDroppableRef } = useDraggable();
+    const { setNodeRef: setDraggableRef } = useDraggable({ id: item.id, data: item });
+    const { setNodeRef: setDroppableRef } = useDroppable({ id: item.id, data: item });
 
     return (
         <div
-            ref={el => setDroppableRef(item.id, el)}
-            className="m-1 p-1 border-2 border-solid border-gray-600 bg-white w-48 h-48"
-            onMouseDown={e => {
-                console.log('onMouseDown');
-                console.log(e);
-
-                // Starts dragging.
-                setDraggingItem(Object.assign({}, item)); //todo: move this into draggable.
+            ref={el => {
+                setDraggableRef(el);
+                setDroppableRef(el);
             }}
+            className="m-1 p-1 border-2 border-solid border-gray-600 bg-white w-48 h-48"
             >
             {item.name}
         </div>
@@ -106,7 +198,7 @@ function Item({ item }) {
 }
 
 function DragOverlay() {
-    const { draggingItem, mousePos } = useDraggable();
+    const { draggingItem, mousePos } = useDragContext();
 
     return draggingItem 
         && <div
@@ -125,16 +217,31 @@ function DragOverlay() {
 }
 
 function App() {
-
-
-    const items1 = [
+    const items1 = [ //todo: these need to be state.
         { id: 1, name: 'item 1' },
     ];
     const items2 = [
         { id: 2, name: 'item 2' },
     ];
     return (
-        <DragContextProvider>
+        <DragContextProvider
+            onDragStart={event => {
+                console.log(`Drag started`);
+                console.log(event);
+            }}
+            onDragOver={event => {
+                console.log(`Drag over`);
+                console.log(event);
+            }}
+            onDragEnd={event => {
+                console.log(`Drag ended`);
+                console.log(event);
+            }}
+            onDragCancel={event => {
+                console.log(`Drag cancelled`);
+                console.log(event);
+            }}
+            >
             <div
                 className="flex flex-row w-full h-screen relative"
                 >
